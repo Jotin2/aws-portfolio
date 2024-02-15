58470d860d09199929f715f4af88f8e16fbc58cc
Joshua Asari <josh.asari@gmail.com>
2024-02-15 14:44:21 -0500

    Changed cors Allow origin and function url

--- website/assets/js/index.js
+++ website/assets/js/index.js
@@ -1,7 +1,7 @@
 // Added Lambda function and HTML for views
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://2y6bsxtyoxgxcqz6bhhfeteghu0bfcdv.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("https://2y6bsxtyoxgxcqz6bhhfeteghu0bfcdv.lambda-url.us-east-1.on.aws/ ");
     let data = await response.json();
     counter.innerHTML = `Views: ${data}`;
 }
