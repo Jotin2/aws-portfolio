f446794f65d8e16279a0659af2c0e01e01daca47
Joshua Asari <josh.asari@gmail.com>
2024-02-16 12:11:52 -0500

    modiefied func.py

--- website/infra/lambda/func.py
+++ website/infra/lambda/func.py
@@ -1,5 +1,11 @@
 import json
 import boto3
+# Set the AWS region
+AWS_REGION = 'us-east-1'  # Replace 'your_aws_region' with your actual AWS region
+
+# Set up the default session with the AWS region
+boto3.setup_default_session(region_name=AWS_REGION)
+
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('cloudresume-test')
 
